203677c25eb3e81d6088b13ed6b8f6e6b15af542
Andrej Popjak <apopjak@gmail.com>
2023-06-25 23:16:56 +0200

    added javascript func

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ var isLogoVisible = true;
 
 window.addEventListener('scroll', function() {
   var currentScrollPos = window.pageYOffset;
+  var logoAppearPos = window.innerHeight * 0.1; // 10% of the window's height
 
-  if (currentScrollPos === 0) { // Check if at the top of the page
+  if (currentScrollPos === 0) {
     logo.style.opacity = '1';
     logo.style.display = 'block';
     isLogoVisible = true;
-  } else if (currentScrollPos > 0 && isLogoVisible) { // Hide logo when scrolling down
+  } else if (currentScrollPos > logoAppearPos && isLogoVisible) {
     logo.style.opacity = '0';
     isLogoVisible = false;
     setTimeout(function() {
       logo.style.display = 'none';
     }, 300);
-  } else if (currentScrollPos === 0 && !isLogoVisible) { // Show logo when scrolling up to the top
+  } else if (currentScrollPos <= logoAppearPos && !isLogoVisible) {
     logo.style.opacity = '1';
     logo.style.display = 'block';
     isLogoVisible = true;
   }
 });
+
 
 // HOME BUTTON ANIMATIO 
 var homeButton = document.querySelector("#home");
@@ -62,3 +64,35 @@ contactButton.addEventListener("click", function(){
   contactSection.scrollIntoView({behavior: "smooth", block: "center"})
 })
 
+/// /////////// under line for skills button
+
+
+function activateUnderline() {
+  var sectionTop = skillsSection.offsetTop - (skillsSection.offsetHeight * 1);
+  var sectionBottom = sectionTop + (skillsSection.offsetHeight * 1);
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+    skillsButton.classList.add('text-decoration-underline');
+  } else {
+    skillsButton.classList.remove('text-decoration-underline');
+  }
+}
+
+window.addEventListener('scroll', activateUnderline);
+
+
+
+function activateUnderline() {
+  var sectionTop = portfolioSection.offsetTop - (portfolioSection.offsetHeight * 1);
+  var sectionBottom = sectionTop + (portfolioSection.offsetHeight * 1);
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+    portfolioButton.classList.add('text-decoration-underline');
+  } else {
+    portfolioButton.classList.remove('text-decoration-underline');
+  }
+}
+
+window.addEventListener('scroll', activateUnderline);
