61dc70b80aeb25b902af0fac9046edfe84a1228f
Andrej Popjak <apopjak@gmail.com>
2023-07-20 09:59:01 +0200

    website update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).scroll(function() {
+$(document).scroll(function () {
   if ($(this).scrollTop() === 0) {
     $("#logo").slideDown();
   } else {
@@ -6,44 +6,25 @@ $(document).scroll(function() {
   }
 });
 
-
 var homeButton = document.querySelector("#home");
 
-homeButton.addEventListener("click", function() {
+homeButton.addEventListener("click", function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
-
-
-
-
 
 // SKILLS BUTTON REDIRECT ANIMATION
 var skillsButton = document.querySelector("#skills");
 
 var skillsSection = document.querySelector("#skills-window");
 
-skillsButton.addEventListener("click", function(){
-  skillsSection.scrollIntoView({behavior: "smooth", block: "center"});
-})
-
-
-// PORTFOLIO BUTTON REDIRECT ANIMATION
-var portfolioButton = document.querySelector("#portfolio");
-var portfolioSection = document.querySelector("#portfolio-window");
-
-portfolioButton.addEventListener("click", function(){
-  var scrollOptions = {
-    behavior: "smooth",
-    block: "center"
-  };
-  portfolioSection.scrollIntoView(scrollOptions);
+skillsButton.addEventListener("click", function () {
+  skillsSection.scrollIntoView({ behavior: "smooth", block: "center" });
 });
 
 // CONTACT BUTTON REDIRECT ANIMATION
 var contactButton = document.querySelector("#contacts");
 var contactSection = document.querySelector("#contact-window");
 
-contactButton.addEventListener("click", function(){
-  contactSection.scrollIntoView({behavior: "smooth", block: "center"})
-})
-
+contactButton.addEventListener("click", function () {
+  contactSection.scrollIntoView({ behavior: "smooth", block: "center" });
+});
